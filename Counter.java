f10e226c4147a6265330a00b0f3adc83710f3e62
BugHunter-X <askzenai@gmail.com>
2025-01-18 21:08:04 +0100

    Add Counter.java file

---
+++ Counter.java
@@ -0,0 +1,11 @@
+public class Counter {
+    private int count = 0; // This is the field declaration
+
+    public void increment() {
+        count++; // This is the increment operation
+    }
+
+    public int getCount() {
+        return count; // This is the getter method
+    }
+}
